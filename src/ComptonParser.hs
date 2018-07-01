b1e7c4c5c17d72153868be83f8302e51313744d2
MyOde <zod@tfwno.gf>
2018-07-01 19:47:41 +0300

    Finished compton parser

--- src/ComptonParser.hs
+++ src/ComptonParser.hs
@@ -16,11 +16,8 @@ import           Text.ParserCombinators.Parsec (Parser, many1, parseFromFile,
                                                 (<|>))
 
 type Entry = (String, Value)
--- TODO You broke it on purpose
-data ComptonConf = ComptonConf { opacityRules :: [Int]
-                               , mainOpacity  :: Int
-                               }
-
+-- TODO Remember to remove all the show derivations - they're not
+-- needed beyond debugging purposes
 data OpacityValue = OpacityValue { opacity    :: Integer
                                  , selector   :: Selector
                                  , comparison :: Comparer
@@ -32,14 +29,21 @@ data RegularValue = RegularValue { r_selector   :: Selector
                                  , r_value      :: String
                                  } deriving Show
 
+data WinType = WinType { name      :: String
+                       , arguments :: [WinTypeArg]
+                       } deriving Show
+
 data Selector = Class | Name | WindowType deriving (Enum, Show)
 data Comparer = Equal | EqualInsens | Like | LikeInsens deriving (Enum, Show)
+-- TODO: Should find out how to limit what I allow from The Value data type
+data WinTypeArg = Fade Value | Shadow Value | Opacity Value | Focus Value deriving Show
 
 data Value
   = Enabled Bool
   | Textual String
   | Numeric Integer
   | Floating Double
+  | WinTypes [WinType]
   | OpacityRules [OpacityValue]
   | RegularRules [RegularValue] deriving Show
 
@@ -49,51 +53,59 @@ parseComptonFile :: String -> IO (Either ParseError [Entry])
 parseComptonFile filePath = parseFromFile comptonConfigParser filePath
 
 comptonConfigParser :: Parser [Entry]
-comptonConfigParser = endBy entryGrouper specialCaseSemicolon
+comptonConfigParser = endBy entryGrouper (char ';' <* whitespace)
 
 whitespace :: Parser ()
 whitespace = skipMany $ oneOf " \t\n"
 
-specialCaseSemicolon :: Parser Char
-specialCaseSemicolon = whitespace *> char ';' <* whitespace
-
-opacityRule :: Parser Entry
-opacityRule = (,) opacityRuleName
-  <$> (try $ string opacityRuleName
-       *> spaces *> char '=' *> spaces *> char '['
-       *> pOpacityArray <* char ']'
-      )
-
 entryGrouper :: Parser Entry
 entryGrouper = opacityRule <|> genericEntry
 
+opacityRule :: Parser Entry
+opacityRule = (,) <$> name <*> opacities
+  where name = try $ string opacityRuleName <* whitespace <* char '='
+        opacities = whitespace *>  char '[' *> pOpacityArray <* char ']'
+
 genericEntry :: Parser Entry
-genericEntry = (,) <$> many1 (noneOf " =:") <*> (spaces *> (parseEntry <|> parseObjectEntry))
+genericEntry = (,)
+  <$> many1 (noneOf " =:")
+  <*> (spaces *> (parseEntry <|> parseWinTypesReal))
 
-parseSimpleValue :: Parser String
-parseSimpleValue = fmap show $ (,) <$> name <*> value where
-  name = whitespace *> many1 (noneOf " =;{}") <* spaces <* char '=' <* spaces
-  value = many1 (noneOf ";") <* char ';' <* whitespace
+parseWindowTypeOption :: String -> Parser Value -> Parser Value
+parseWindowTypeOption name valueParser =
+  ((try $ string name) *> whitespace *> char '='
+   *> whitespace *> valueParser <* char ';'
+      )
 
-parseObjectNotation :: Parser String
-parseObjectNotation = fmap show $ (,) <$> name <*> value where
-  name = many1 (noneOf " :\t{}") <* spaces <* char ':' <* whitespace <* char '{'
-  value = many1 parseSimpleValue <* whitespace <* char '}' <* char ';' <* whitespace
+win_Fade = Fade <$> parseWindowTypeOption "fade" pEnabled
+win_Shadow = Shadow <$> parseWindowTypeOption "shadow" pEnabled
+win_Opacity = Opacity <$> parseWindowTypeOption "opacity" pFloater
+win_Focus = Focus <$> parseWindowTypeOption "focus" pEnabled
 
--- TODO Still not fully parsing the JSON object notation
-parseObjectEntry :: Parser Value
-parseObjectEntry = fmap (\cont -> Textual (cont >>= id))
-  $ char ':' *> whitespace *> char '{' *> whitespace
-  *> many1 parseObjectNotation
+parseWinType :: Parser [WinTypeArg]
+parseWinType = whitespace *> many1 (argumentParser <* whitespace)
+  where argumentParser = win_Shadow <|> win_Opacity <|> win_Fade <|> win_Focus
+
+parseWinTypes :: Parser WinType
+parseWinTypes = WinType <$> name <*> winTypes
+  where name = many1 (noneOf " :\t{}")
+        winTypes = (whitespace *> char ':' *> whitespace *> char '{' *> parseWinType <* whitespace <* char '}' <* char ';' <* whitespace)
+
+parseWinTypesReal :: Parser Value
+parseWinTypesReal = WinTypes <$>
+  (char ':' *> whitespace *> char '{' *> whitespace
+  *> (many1 parseWinTypes)
   <* whitespace <* char '}'
+  )
 
 parseEntry :: Parser Value
-parseEntry = char '=' *> spaces *> content where
-  content = pArray <|> pTrue <|> pFalse <|> pFloater
-            <|> pInteger <|> pNatural <|> pString
+parseEntry = char '=' *> spaces *> content
+  where content = pArray <|> pEnabled <|> pFloater
+            <|> pInteger <|> pNatural <|> pTextual
 
-pString :: Parser Value
-pString = fmap Textual
+-- TODO I really like the trip that I had while naming these things
+pTextual :: Parser Value
+pTextual = fmap Textual
   $ spaces *> char '"' *> many1 (noneOf "\"") <* char '"'
 
 pArray :: Parser Value
@@ -112,13 +124,13 @@ opaFromRegular opacity RegularValue{..} =
   OpacityValue opacity r_selector r_comparison r_value
 
 pOpacityLine :: Parser OpacityValue
-pOpacityLine = (\opa val -> (opaFromRegular (read opa) val)) <$> pOpa <*> pVal
+pOpacityLine = opaFromRegular <$> (read <$> pOpa) <*> pVal
   where pOpa = whitespace *> char '"' *> try naturalNumber
         pVal = char ':' *> regularLineBase <* char '"'
 
 regularLineBase :: Parser RegularValue
 regularLineBase = RegularValue <$> parseSelector <*> comp <*> value
-  where comp = spaces *> stringComparer <* spaces
+  where comp = whitespace *> stringComparer <* whitespace
         value = char '\'' *> many1 (noneOf "'") <* char '\''
 
 pRegularLine :: Parser RegularValue
@@ -139,43 +151,47 @@ pFloat = fmap rd $ (++) <$> pInt <*> decimal
   where rd      = read :: String -> Double
         decimal = option "" $ (:) <$> char '.' <*> naturalNumber
 
--- TODO Find how to simplify this shi*
+-- TODO Find how to simplify this shi*. Pretty sure I'll just rewrite the
+-- dependent stuff to not need something weird like this
 tryAndClassify :: String -> a -> Parser a
 tryAndClassify tryWord successReturn = do
   try $ string tryWord
   return successReturn
 
+tryString :: String -> Parser String
+tryString word = try $ string word
+
 pNatural :: Parser Value
 pNatural = fmap Numeric $ read <$> try naturalNumber
-
 pInteger :: Parser Value
 pInteger = fmap Numeric $ read <$> try pInt
-
 pFloater :: Parser Value
 pFloater = Floating <$> try pFloat
+pEnabled :: Parser Value
+pEnabled = Enabled <$> parseBoolean
 
 stringComparer :: Parser Comparer
 stringComparer = pEqualComp <|> pLikeComp <|> pEqualInsens <|> pLikeInsens
-pEqualComp = tryAndClassify "=" Equal
-pLikeComp = tryAndClassify "*=" Like
-pLikeInsens = tryAndClassify "*?=" LikeInsens
-pEqualInsens = tryAndClassify "?=" EqualInsens
+  where pEqualComp = Equal <$ tryString "="
+        pLikeComp = Like <$ tryString "*="
+        pLikeInsens = LikeInsens <$ tryString "*?="
+        pEqualInsens = EqualInsens <$ tryString "?="
 
 parseSelector :: Parser Selector
 parseSelector = pName <|> pClass <|> pWindowType
-pName = tryAndClassify "name" Name
-pClass = tryAndClassify "class_g" Class
-pWindowType = tryAndClassify "window_type" WindowType
+  where pName = Name <$ tryString "name"
+        pClass = Class <$ tryString "class_g"
+        pWindowType = WindowType <$ tryString "window_type"
 
-pBool :: Parser Value
-pBool = pTrue <|> pFalse
-pTrue = tryAndClassify "true" (Enabled True)
-pFalse = tryAndClassify "false" (Enabled False)
+parseBoolean :: Parser Bool
+parseBoolean = pTrue <|> pFalse
+  where pTrue = True <$ tryString "true"
+        pFalse = False <$ tryString "false"
 
 -- ++++++++++++++++++++
 -- ADHOC TEST FUNCTIONS
 -- ++++++++++++++++++++
-log_file_path = "/home/bmiww/comproller_log_parsed"
+log_file_path = "/home/bmiww/comproller_log_parsed2"
 config_file_path = "/home/bmiww/.config/compton.conf_"
 
 testParser :: IO ()
