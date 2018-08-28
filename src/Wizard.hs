cfa29525d2f3b5643970b9b8332e3947d7489b11
MyOde <zod@tfwno.gf>
2018-08-29 01:03:29 +0200

    Replaced the list used for Compton data with a Map.

--- src/Wizard.hs
+++ src/Wizard.hs
@@ -1,8 +1,8 @@
 module Wizard where
 
 import qualified Compton.Static as CS
-import           Compton.Types  (Entry, Value (..))
-import           Data.List     (filter)
+import           Compton.Types   (ComptonMap, Entry, Value (..))
+import           Data.Map.Strict (filterWithKey, toList)
 
 data WizardStep
   = Initial
@@ -24,7 +24,8 @@ changeNumeric = ("Change numeric", ChooseNumberEntry)
 changeFlag :: (String, WizardStep)
 changeFlag = ("Toggle flag", ChooseFlagEntry)
 
-wizardStep :: WizardStep -> [Entry] -> [(String, WizardStep)]
+-- TODO You will probably update this to use some state monad?
+wizardStep :: WizardStep -> ComptonMap -> [(String, WizardStep)]
 wizardStep Initial _ =
   [ changeFlag
   , changeNumeric
@@ -33,17 +34,20 @@ wizardStep Initial _ =
   ]
 
 wizardStep ChooseFlagEntry entries = dynEntries ++ [saveAndExit, exit]
-  where entriesWithValues = filter
-                            (\(name, _) -> any
+  -- TODO quick and dirty patch to filter by the keys in the compton map
+  where entriesWithValues = filterWithKey
+                            (\name _ -> any
                               (\static -> static == name)
                               CS.booleanEntries) entries
         dynEntries =
           fmap
           (\(entryName, entryValue) ->
+          -- (\entryName entryValue ->
              let displayName = (dirtyBoolExtract entryValue) ++ " " ++ entryName in
                (displayName, ChangeFlag entryName)
           )
-          entriesWithValues
+          -- TODO also quite dirty - maybe its possible to fmap or traverse/fold directly into a list type
+          $ toList entriesWithValues
 
 dirtyBoolExtract :: Value -> String
 dirtyBoolExtract (Enabled False) = "T"
